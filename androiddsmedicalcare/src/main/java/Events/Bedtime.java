844c46c5f41d484a0a07ad0362fe910839d5bb60
wendywang <wangwenusa11@gmail.com>
2015-06-13 12:16:55 -0700

    finished getter and setter in Events class

--- androiddsmedicalcare/src/main/java/Events/Bedtime.java
+++ androiddsmedicalcare/src/main/java/Events/Bedtime.java
@@ -5,15 +5,8 @@ import java.sql.Time;
 /**
  * Created by wendywang on 15-06-13.
  */
-public class Bedtime extends Events{
+public class Bedtime {
 
-    private String name;
-    private Time time;
-
-    public Bedtime(String name, Time time){
-        this.name = name;
-        this.time = time;
-    }
 
 
 
--- androiddsmedicalcare/src/main/java/Events/Events.java
+++ androiddsmedicalcare/src/main/java/Events/Events.java
@@ -1,14 +1,39 @@
 package Events;
 
+import java.sql.Time;
+
 /**
  * Created by wendywang on 15-06-13.
  */
 
 
 public class Events {
-    public Events(){
 
+    private String name;
+    private Time time;
+
+    public Events(String name, Time time){
+        this.name = name;
+        this.time = time;
     }
+
+    public void setName(String eventName){
+        name = eventName;
+    }
+
+    public void setTime(Time eventTime){
+        time = eventTime;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Time getTime(){
+        return time;
+    }
+
+
 
 
 }
