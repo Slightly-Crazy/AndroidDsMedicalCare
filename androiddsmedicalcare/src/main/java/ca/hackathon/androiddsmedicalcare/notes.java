04ed5b5be7cb9954eac405905e37e531983522ab
wendywang <wangwenusa11@gmail.com>
2015-06-13 19:01:24 -0700

    rest

--- androiddsmedicalcare/src/main/java/ca/hackathon/androiddsmedicalcare/notes.java
+++ androiddsmedicalcare/src/main/java/ca/hackathon/androiddsmedicalcare/notes.java
@@ -1,9 +1,11 @@
 package ca.hackathon.androiddsmedicalcare;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class notes extends ActionBarActivity {
@@ -19,6 +21,13 @@ public class notes extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_notes, menu);
         return true;
+    }
+
+    public void onclick_okbutton(View view){
+        Intent intent = new Intent();
+        intent.setClass(this, MainActivity.class);
+        //intent.putExtra("EXTRA_ID", "SOME DATAS");
+        startActivity(intent);
     }
 
     @Override
